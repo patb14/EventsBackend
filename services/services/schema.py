878916c34c0cfc486c3ee18f0e853f8d5ef81493
patrickbrownrigg <patrick@deposify.com>
2023-06-02 23:00:19 -0400

    lil graphql tings

--- services/services/schema.py
+++ services/services/schema.py
@@ -1,13 +1,32 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from events.models import Event
+from events.models import Event, Session, Gender, EventLocation
 
+# TODO: STARTING HERE: https://docs.graphene-python.org/projects/django/en/latest/tutorial-relay/#schema
 
 class EventType(DjangoObjectType):
     class Meta:
         model = Event
-        fields = ("id", "name", "start_date", "end_date", "location", "gender", "times", "cost")
+        fields = ("id", "name", "start_date", "end_date", "location", "gender", "sessions", "cost")
+
+
+class SessionType(DjangoObjectType):
+    class Meta:
+        model = Session
+        fields = ("id", "name", "start_time", "end_time")
+
+
+class GenderType(DjangoObjectType):
+    class Meta:
+        model = Gender
+        fields = ("id", "gender")
+
+
+class EventLocationType(DjangoObjectType):
+    class Meta:
+        model = EventLocation
+        fields = ("id", "name", "address ")
 
 
 class Query(graphene.ObjectType):
