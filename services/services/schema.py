038d0e7c023068988a58d00c544286fc7c4f6938
patrickbrownrigg <patrick@deposify.com>
2023-06-02 23:14:31 -0400

    django-filter

--- services/services/schema.py
+++ services/services/schema.py
@@ -1,39 +1,47 @@
 import graphene
 from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 
 from events.models import Event, Session, Gender, EventLocation
 
-# TODO: STARTING HERE: https://docs.graphene-python.org/projects/django/en/latest/tutorial-relay/#schema
+
+# TODO: We probably want a custom object type. Just so theres default exclude fields like timestamps, etc
 
 class EventType(DjangoObjectType):
     class Meta:
         model = Event
+        filter_fields = ['name', 'location']
         fields = ("id", "name", "start_date", "end_date", "location", "gender", "sessions", "cost")
+        interfaces = (graphene.relay.Node,)
 
 
 class SessionType(DjangoObjectType):
     class Meta:
         model = Session
+        filter_fields = ['name']
         fields = ("id", "name", "start_time", "end_time")
+        interfaces = (graphene.relay.Node,)
 
 
 class GenderType(DjangoObjectType):
     class Meta:
         model = Gender
+        filter_fields = ['gender']
         fields = ("id", "gender")
+        interfaces = (graphene.relay.Node,)
 
 
 class EventLocationType(DjangoObjectType):
     class Meta:
         model = EventLocation
+        filter_fields = ['name']
         fields = ("id", "name", "address ")
+        interfaces = (graphene.relay.Node,)
 
 
 class Query(graphene.ObjectType):
-    all_events = graphene.List(EventType)
-
-    def resolve_all_events(root, info):
-        return Event.objects.all()
+    event = graphene.relay.Node.Field(EventType)
+    all_events = DjangoFilterConnectionField(EventType)
 
 
 schema = graphene.Schema(query=Query)
